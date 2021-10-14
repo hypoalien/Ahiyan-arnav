6cd41897eba5d6c467d22a7341a310f4af9e4699
hypoalien <hypoalien@gmail.com>
2021-10-15 02:02:29 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -89,8 +89,8 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("October 25, 2021 15:37:25").getTime();
-        //-date: "Mar 25 2021",
+        var finalDate =  new Date("November 4, 2021 15:37:25").getTime();
+        //-date: "Nov 5 2021",
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
